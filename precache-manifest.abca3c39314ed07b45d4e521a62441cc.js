85c5e57651239143aa99754778cdb73c937d3954
YannosSidiras <ysidiras@gmail.com>
2018-11-02 20:46:34 +0200

    Updates

---
+++ precache-manifest.abca3c39314ed07b45d4e521a62441cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "%7Baccount%7D.github.io/%7Bname%7D/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a18702a7d6a408e6b7e",
+    "url": "%7Baccount%7D.github.io/%7Bname%7D/static/js/runtime~main.7a18702a.js"
+  },
+  {
+    "revision": "2ca49a90139deffef837",
+    "url": "%7Baccount%7D.github.io/%7Bname%7D/static/js/main.2ca49a90.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "%7Baccount%7D.github.io/%7Bname%7D/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "2ca49a90139deffef837",
+    "url": "%7Baccount%7D.github.io/%7Bname%7D/static/css/main.91c49d2b.chunk.css"
+  },
+  {
+    "revision": "6cb92115b21803be8d742d9c5c2a28d2",
+    "url": "%7Baccount%7D.github.io/%7Bname%7D/index.html"
+  }
+];
